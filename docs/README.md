7ebed928c25daa6ba32b40c5df18ee2af8f6d136
adviknagpal15 <adviknagpal.10@gmail.com>
2026-09-02 18:37:47 +0800

    Level 0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Dash User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Dash.java
+++ src/main/java/Dash.java
@@ -1,7 +1,7 @@
-public class Duke {
+public class Dash {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
+                + "|  _ \\ _ _ _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
