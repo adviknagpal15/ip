bc7afa9be8d73d7be6a6a51f98d63f7852c0add3
adviknagpal15 <adviknagpal.10@gmail.com>
2026-09-02 18:37:47 +0800

    Level 0

---
+++ src/main/java/Dash.java
@@ -0,0 +1,12 @@
+public class Dash {
+    public static void main(String[] args) {
+        String banner =
+                " ____              _     \n"
+                        + "|  _ \\  __ _ ___| |__  \n"
+                        + "| | | |/ _` / __| '_ \\ \n"
+                        + "| |_| | (_| \\__ \\ | | |\n"
+                        + "|____/ \\__,_|___/_| |_|\n";
+
+        System.out.println(banner);
+    }
+}
