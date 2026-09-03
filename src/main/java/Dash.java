8fd01bcf356a8593b0adfb4c86eee0ddd79c8c2b
adviknagpal15 <adviknagpal.10@gmail.com>
2026-09-03 21:37:44 +0800

    Level 0

--- src/main/java/Dash.java
+++ src/main/java/Dash.java
@@ -8,5 +8,15 @@ public class Dash {
                         + "|____/ \\__,_|___/_| |_|\n";
 
         System.out.println(banner);
+
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm Dash.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+
     }
 }
