2569a009e91bad8790f6eeed79a236d0c4f9eedb
adviknagpal15 <adviknagpal.10@gmail.com>
2026-09-03 21:41:45 +0800

    Level 1

--- src/main/java/Dash.java
+++ src/main/java/Dash.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
+/**
+ * A chatbot that echoes commands until the user says goodbye.
+ */
 public class Dash {
     public static void main(String[] args) {
         String banner =
@@ -15,8 +20,20 @@ public class Dash {
         System.out.println("Hello! I'm Dash.");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
 
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println(line);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(" " + command);
+            System.out.println(line);
+        }
     }
 }
